9163d758b446553b6a2b35a298bcf6e35df845f4
jaweed3 <wedjaw22@gmail.com>
2026-07-01 06:27:16 +0700

    feat: security middleware refactor

---
+++ app/middleware/security.py
@@ -0,0 +1,12 @@
+from starlette.middleware.base import BaseHTTPMiddleware, Callable
+from starlette.requests import Request
+from starlette.responses import Response
+
+
+class SecurityHeadersMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: Callable) -> Response:
+        response = await call_next(request)
+        response.headers["X-Content-Type-Options"] = "nosniff"
+        response.headers["X-Frame-Options"] = "DENY"
+        response.headers["Cache-Control"] = "no-store"
+        return response
